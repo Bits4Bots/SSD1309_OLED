ff35dadf001c3c9b7c8fb14a5dc94421a1e0b91e
Bits4Bots LLC <bits4bots@gmail.com>
2025-09-07 13:29:23 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    // Set OLED I2C address using dropdown (0x3C = 60)
+    // Set OLED I2C address (0x3C = 60)
     SSD1309_OLED.setAddress(SSD1309_OLED.OLED_I2C_Address.addr_3C)
 
     // Initialize OLED at 128x64
@@ -12,7 +12,7 @@ basic.forever(function () {
     SSD1309_OLED.writeString("SSD1309 OLED OK")
     basic.pause(2000)
 
-    // Scroll text at 100 ms
+    // Scroll a message
     SSD1309_OLED.scrollText("Hello from Bits4Bots SSD1309 OLED!", 100)
 
     basic.pause(2000)
